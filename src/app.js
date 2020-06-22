d9c1b224ac892ae305d7674ee8909ae1b300eefe
OfficiallyLost <farooqhaseeb49@gmail.com>
2020-06-22 23:53:38 +0100

    add routes in app.js

--- src/app.js
+++ src/app.js
@@ -6,9 +6,17 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-	res.render('home', {
-		joke: 'How did you find this page again? Im lost and trying to find my way out of it!'
-	});
+	res.render('home');
+	console.log('someone accessed the home page');
+});
+app.get('/github', (req, res) => {
+	res.redirect('https://github.com/OfficiallyLost/');
+	console.log('someone accessed the github');
+});
+
+app.get('/discord', (req, res) => {
+	res.redirect('https://discord.gg/FWTRPS9');
+	console.log('someone accessed discord');
 });
 
 app.listen(8000, () => {
@@ -16,6 +24,7 @@ app.listen(8000, () => {
 });
 
 app.get('*', (req, res) => {
+	console.log('someone hit the 404');
 	res.render('404', {
 		joke: "Are you Lost? I can't find this page, it doesn't exist!"
 	});
