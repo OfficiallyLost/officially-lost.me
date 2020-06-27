8cb78768b55d43e7406c0a788cdb2585b4fbc708
OfficiallyLost <farooqhaseeb49@gmail.com>
2020-06-27 21:46:41 +0100

    add login stuff

--- src/app.js
+++ src/app.js
@@ -2,18 +2,72 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const db = require('./database/index');
-const Urls = require('./database/models/url');
-const id = require('shortid');
+const user = require('./database/models/user');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
+const init = require('./auth/passport');
+
 db.then(() => console.log('Successfully connected the database')).catch((e) => console.log(e));
 process.on('unhandledRejection', () => {});
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+init(passport, (username, email) => {
+	return users.find((e) => e.email === email);
+});
+const LocalStrategy = require('passport-local').Strategy
+app.use(
+	session({
+		secret: 'idk',
+		resave: false,
+		saveUninitialized: false
+	})
+);
 
 app.get('/', (req, res) => {
 	res.render('home');
 	console.log('someone accessed the home page');
 });
+
+app.get('/login', (req, res) => {
+	res.render('login');
+});
+
+app.get('/create', (req, res) => {
+	res.render('create');
+});
+
+app.post('/create', async (req, res) => {
+	try {
+		const password = await bcrypt.hash(req.body.password, 15);
+		console.log(req.body);
+		const pers = await user.create({
+			id: Date.now().toString(),
+			name: req.body.username,
+			email: req.body.email,
+			password: password
+		});
+		console.log(pers);
+		res.redirect('/thank-u');
+	} catch (e) {
+		console.log(e);
+	}
+});
+
+app.post(
+	'/login',
+	passport.authenticate('local', {
+		successRedirect: '/',
+		failureRedirect: '/login',
+		failureFlash: true
+	})
+);
 
 app.get('/github', (req, res) => {
 	res.redirect('https://github.com/OfficiallyLost/');
