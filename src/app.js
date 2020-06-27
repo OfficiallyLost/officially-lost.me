7cafdcff757770d82eb3550c9be759415976d6e8
OfficiallyLost <farooqhaseeb49@gmail.com>
2020-06-27 14:52:02 +0100

    remove url stuff

--- src/app.js
+++ src/app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
+const db = require('./database/index');
+const Urls = require('./database/models/url');
+const id = require('shortid');
+db.then(() => console.log('Successfully connected the database')).catch((e) => console.log(e));
+process.on('unhandledRejection', () => {});
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
 	res.render('home');
 	console.log('someone accessed the home page');
 });
+
 app.get('/github', (req, res) => {
 	res.redirect('https://github.com/OfficiallyLost/');
 	console.log('someone accessed the github');
--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-
-module.exports = mongoose.connect('mongodb://localhost/site', {
+module.exports = mongoose.connect(`mongodb://localhost/site`, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
 });
