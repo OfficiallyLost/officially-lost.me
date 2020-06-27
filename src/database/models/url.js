094adc0153c718be6317d8a0342e4b9b51b7249a
OfficiallyLost <farooqhaseeb49@gmail.com>
2020-06-27 14:51:41 +0100

    update model

--- src/database/models/url.js
+++ src/database/models/url.js
@@ -2,10 +2,8 @@ const mongoose = require('mongoose');
 const id = require('shortid');
 
 const url = new mongoose.Schema({
-	url: {
-		short: { type: String, default: id.generate, required: true },
-		full: { type: String, required: true }
-	}
+	short: { type: String, required: true },
+	full: { type: String, required: true }
 });
 
 module.exports = mongoose.model('url', url);
