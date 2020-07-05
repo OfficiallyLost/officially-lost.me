3ff0b33036bdf3d6a84c0e52582c43a364e41fac
haseeb <farooqhaseeb49@gmail.com>
2020-07-05 21:45:22 +0100

    add sessionID for later use

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const user = mongoose.Schema({
 	id: Number,
 	username: String,
-	password: String
+	password: String,
+	sessionID: String
 });
 
 module.exports = mongoose.model('user', user);
